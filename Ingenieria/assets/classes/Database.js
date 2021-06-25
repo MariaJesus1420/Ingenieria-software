c418be77fd1b13146fdaefaaafb90f2ef71947fc
JirenJerry <riverderio@gmail.com>
2021-06-25 17:23:41 -0600

    Fix login email

--- Ingenieria/assets/classes/Database.js
+++ Ingenieria/assets/classes/Database.js
@@ -24,19 +24,23 @@ class DataBase {
         });
         return result;
     }
-    async loginEmailPassword(email, password) {
+    async loginEmailPassword(email, password, session) {
         let user;
-        firebase.auth().signInWithEmailAndPassword(email, password)
-            .then((userCredential) => {
-                // Signed in
-                user = userCredential.user;
+        firebase.auth().setPersistence(firebase.auth.Auth.Persistence[session])
+            .then(() => {
+                // Existing and future Auth states are now persisted in the current
+                // session only. Closing the window would clear any existing state even
+                // if a user forgets to sign out.
                 // ...
+                // New sign-in will be persisted with session persistence.
+                return firebase.auth().signInWithEmailAndPassword(email, password);
             })
             .catch((error) => {
-                let errorCode = error.code;
-                let errorMessage = error.message;
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
             });
-        return user;
+        return Promise.resolve(user);
     }
 
     async registroEmailPassword(email, password) {
