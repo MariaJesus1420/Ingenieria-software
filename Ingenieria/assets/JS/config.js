4bf715fd5a6330dcdb5af53449cb4bc9b1f999ba
JirenJerry <riverderio@gmail.com>
2021-06-30 11:24:14 -0600

    mas cosas del config

--- Ingenieria/assets/JS/config.js
+++ Ingenieria/assets/JS/config.js
@@ -5,6 +5,30 @@ console.log('PENE');
 
 $(function () {
     console.log("READY");
-    $('#schedule').jqs();
+    $('#schedule').jqs({
+        daysList: [
+            'Lunes',
+            'Martes',
+            'Miercoles',
+            'Jueves',
+            'Viernes',
+            'Sabado',
+            'Domingo'
+          ],
+  
+        periodColors: [
+
+            ['rgba(200, 0, 0, 0.5)', '#f00', '#000'],
+            ['rgba(0, 200, 0, 0.5)', '#0f0', '#000'],
+
+        ],
+        periodTitle: 'No title',
+        periodBackgroundColor: 'rgba(200, 0, 0, 0.5)',
+        periodBorderColor: '#f00',
+        periodTextColor: '#00',
+        periodRemoveButton: 'Eliminar',
+        periodDuplicateButton: 'Duplicar',
+        periodTitlePlaceholder: 'Titulo personalizado'
+    });
     console.log("HOLAAA");
 });
